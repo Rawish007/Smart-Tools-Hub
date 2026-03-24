a517dc05336147dc3c7e005659d8a54d5aa264af
Rawish007 <134200704+Rawish007@users.noreply.github.com>
2026-03-24 23:52:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, send_file
 from PIL import Image
-import os
+import random
+import string
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,19 +11,22 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-# TOOLS
+# PROFIT
 @app.route("/profit")
 def profit():
     return render_template("profit.html")
 
+# CURRENCY
 @app.route("/currency")
 def currency():
     return render_template("currency.html")
 
+# UNIT
 @app.route("/unit")
 def unit():
     return render_template("unit.html")
 
+# LOAN
 @app.route("/loan")
 def loan():
     return render_template("loan.html")
@@ -36,6 +41,36 @@ def image_compressor():
         img.save(path, optimize=True, quality=30)
         return send_file(path, as_attachment=True)
     return render_template("image.html")
+
+# AGE CALCULATOR
+@app.route("/age", methods=["GET", "POST"])
+def age():
+    result = ""
+    if request.method == "POST":
+        birth = request.form["birth"]
+        today = datetime.today()
+        birth_date = datetime.strptime(birth, "%Y-%m-%d")
+        age = today.year - birth_date.year
+        result = f"Your age is {age} years"
+    return render_template("age.html", result=result)
+
+# PASSWORD GENERATOR
+@app.route("/password")
+def password():
+    chars = string.ascii_letters + string.digits
+    password = ''.join(random.choice(chars) for _ in range(10))
+    return render_template("password.html", password=password)
+
+# BMI CALCULATOR
+@app.route("/bmi", methods=["GET", "POST"])
+def bmi():
+    result = ""
+    if request.method == "POST":
+        w = float(request.form["weight"])
+        h = float(request.form["height"]) / 100
+        bmi = w / (h*h)
+        result = f"Your BMI is {round(bmi,2)}"
+    return render_template("bmi.html", result=result)
 
 # EXTRA PAGES
 @app.route("/about")
