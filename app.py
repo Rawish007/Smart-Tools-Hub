9f4ab81080d8dd89d3a3f8ee35fa00d7d0587714
Rawish007 <134200704+Rawish007@users.noreply.github.com>
2026-03-24 17:21:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
+from PIL import Image
+import os
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/profit")
-def profit():
-    return render_template("profit.html")
-
-@app.route("/currency")
-def currency():
-    return render_template("currency.html")
-
-@app.route("/unit")
-def unit():
-    return render_template("unit.html")
-
-@app.route("/loan")
-def loan():
-    return render_template("loan.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/image-compressor", methods=["GET", "POST"])
+def image_compressor():
+    if request.method == "POST":
+        file = request.files["image"]
+        if file:
+            img = Image.open(file)
+            img = img.convert("RGB")
+            img.save("compressed.jpg", optimize=True, quality=50)
+            return send_file("compressed.jpg", as_attachment=True)
+    return render_template("image.html")
